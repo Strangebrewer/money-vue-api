ba19908534767df5153d21a39977bceb84dd8a63
Strangebrewer <BKAShambala@gmail.com>
2019-09-15 01:33:52 -0600

    refining headers

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import './connection';
-import cors from 'cors';
+// import cors from 'cors';
 const app = express();
 import routes from './routes';
 const PORT = 3000;
@@ -10,7 +10,14 @@ import bodyParser from 'body-parser';
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
+   res.header("Access-Control-Allow-Methods", "*");
+   next();
+});
 
 app.use(routes);
 
