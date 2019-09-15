c6732d0ac8f3b1cc5150727a584c796eb0349777
Strangebrewer <BKAShambala@gmail.com>
2019-09-15 01:09:52 -0600

    change MongoDBURI string

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const { DB_USERNAME, DB_PASSWORD, DB_CLUSTER, MONGODB_URI } = process.env;
 
-const uri = MONGODB_URI || `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/money_vue?retryWrites=true`;
+const uri = MONGODB_URI || `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/money_vue?retryWrites=true&w=majority`;
 
 mongoose.connect(uri, { useNewUrlParser: true, useFindAndModify: false })
    .then(() => console.log('Connected to Mongo'))
