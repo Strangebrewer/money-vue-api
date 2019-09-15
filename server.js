fd09d4964dfc45a2ae17fbb2e52f822b72d70a73
Strangebrewer <BKAShambala@gmail.com>
2019-09-15 01:36:43 -0600

    refining headers

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.text());
 // app.use(cors());
 
 app.use((req, res, next) => {
-   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Origin", "http://localhost:8080");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Authorization, Accept");
    res.header("Access-Control-Allow-Methods", "*");
    next();
